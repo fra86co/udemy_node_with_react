4a67db2e315a64ac7f2d6633fb536dc829248dbd
fra86co <francesco.deluca@external-rumbo.com>
2018-01-29 14:40:23 +0100

    added passport js and google auth

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({bye:'bye'});
-});
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
 
