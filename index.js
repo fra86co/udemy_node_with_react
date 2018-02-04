e57407b7d705a8c5245563dc15d3fc3eb309ad3d
fra86co <francesco.deluca@external-rumbo.com>
2018-02-04 21:35:51 +0100

    added passport js and google auth

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+const passport = require('passport')
 const authRoutes = require('./routes/authRoutes');
+
 
 mongoose.connect(keys.mongoURI)
 
+
 const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
-
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,29 @@
 const passport = require('passport');
 
-module.exports = (app) => {
-    app.get('/auth/google',
+module.exports = app => {
+    app.get(
+        '/auth/google',
         passport.authenticate('google', {
-            scope: ['profile', 'email']
-        }));
+            scope: ['profile', 'email'],
+            prompt: 'consent'
+        })
+    );
 
-    app.get('/auth/google/callback',
-        passport.authenticate('google')
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+        (err, req, res, next) => {
+            if (err.name === 'TokenError') {
+                console.log(err);
+                res.redirect('/auth/tokenerror'); // redirect them back to the login page
+            }
+            else {
+                console.log(err);
+                res.redirect('/auth/error');
+            }
+        },
+        (req, res) => { // On success, redirect back to '/'
+            res.redirect('/');
+        }
     );
 };
