751af17181d13780c10e7ecc6656ae3141fca652
fra86co <francesco.deluca@external-rumbo.com>
2018-02-11 16:28:21 +0100

    Added routes to show user and logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -26,4 +26,13 @@ module.exports = app => {
             res.redirect('/');
         }
     );
+
+    app.get('/api/current_user', (req,res)=>{
+        res.send(req.user);
+    })
+
+    app.get('/api/logout', (req, res)=>{
+        req.logout()
+        res.send(req.user);
+    });
 };
