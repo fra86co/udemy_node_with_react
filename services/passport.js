fd7bc4b7c6662c0402750fcda8e8f4962c6f53fe
fra86co <francesco.deluca@external-rumbo.com>
2018-02-25 15:23:05 +0100

    Refactoring with async/await

--- services/passport.js
+++ services/passport.js
@@ -24,26 +24,17 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
+        async (accessToken, refreshToken, profile, done) => {
 
-            console.log('profile', profile);
+            const existingUser = await User.findOne({googleId: profile.id});
+            if (existingUser) {
+                console.log("Already exists " + profile.id);
+                done(null, existingUser);
+            }
 
-            User.findOne({googleId: profile.id})
-                .then((existingUser) => {
-                    if (existingUser) {
-                        console.log("Already exists " + profile.id);
-                        done(null, existingUser);
-                    }
-                    else {
-                        new User({googleId: profile.id})
-                            .save()
-                            .then(user => {
-                                console.log('user instance saved');
-                                done(null, user);
-                            });
-                    }
-                });
-
+            const user = await new User({googleId: profile.id}).save();
+            console.log('user instance saved');
+            done(null, user);
         }
     )
 );
